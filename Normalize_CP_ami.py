de219399afd446dbcf2c5811a36db641a1e38ae9
Diego Camacho <dcamacho@saguarobio.com>
2025-07-30 10:16:22 -0400

    Compatibility with binned images

--- Normalize_CP_ami.py
+++ Normalize_CP_ami.py
@@ -35,7 +35,7 @@ def concatenate_csv_from_s3(bucket_name, plates, times, base_folder_path, output
 
     for plate in plates:
         logger.info(f"Processing plate ID: {plate}")
-        filtered_plateMap = read_csv_from_s3(bucket_name, f"{base_folder_path}/Plate_{plate}_PlateMap.csv",s3)
+        filtered_plateMap = read_csv_from_s3(bucket_name, f"{base_folder_path}/Plate_{plate.lstrip('binned/')}_PlateMap.csv",s3)
         filtered_plateMap = filtered_plateMap[['Metadata_Compound', 'Metadata_ConcLevel', 'Metadata_Well', 'Metadata_Plate']]# plate map 
         filtered_plateMap["Metadata_Compound"] = filtered_plateMap["Metadata_Compound"].apply(lambda x: str(x).upper())
         for time in times:
