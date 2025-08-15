5aa937dba599bcb3337774ae84015963cb5b1290
Diego Camacho <dcamacho@saguarobio.com>
2025-08-15 17:04:13 -0400

    Fixed output data converion.

--- Cellpose_EfficientNet_embeddings.py
+++ Cellpose_EfficientNet_embeddings.py
@@ -165,7 +165,7 @@ def main(args):
     s3_input_path = f"s3://{args.bucket_input}/{args.load_data_key}"
     logging.info(f"Reading load_data CSV from {s3_input_path}")
     try:
-        load_data = pd.read_csv(s3_input_path).iloc[0:99,:]
+        load_data = pd.read_csv(s3_input_path).iloc[0:50,:]
     except Exception as e:
         logging.error(f"Failed to read input CSV from S3. Error: {e}")
         return # Exit if we can't load the main file
@@ -244,7 +244,8 @@ def main(args):
     # --- Save Final Results ---
     s3_output_path = f"s3://{args.bucket_output}/{args.out_data_path}"
     logging.info(f"Saving final aggregated data to {s3_output_path}")
-    meta_data.to_parquet(s3_output_path, engine='pyarrow')
+    #meta_data.to_parquet(s3_output_path, engine='pyarrow')
+    meta_data.to_parquet(args.out_data_path, engine='pyarrow')
     
     logging.info("Script finished successfully.")
 
