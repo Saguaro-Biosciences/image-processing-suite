05d74508e89826fd8e72a37b439a18e8d26082a8
Diego Camacho <dcamacho@saguarobio.com>
2025-08-18 20:05:56 -0400

    Rethough data ingestion.

--- Cellpose_GPU_arhi.py
+++ Cellpose_GPU_arhi.py
@@ -285,7 +285,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the optimized cell image analysis pipeline.")
     
-    parser.add_argument('--max-workers', type=int, default=os.cpu_count() - 1, help='Number of parallel CPU I/O processes.')
+    parser.add_argument('--max-workers', type=int, default=os.cpu_count() * 2, help='Number of parallel CPU I/O processes.')
     parser.add_argument('--bucket-input', type=str, required=True, help='Name of the S3 bucket for input data.')
     parser.add_argument('--load-data-key', type=str, required=True, help='S3 key to the load_data.csv file.')
     parser.add_argument('--meta-data-key', type=str, required=True, help='S3 key to the meta_data.csv file.')
